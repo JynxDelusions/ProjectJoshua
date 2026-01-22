3805c03ee94e034bfa9255342bc3188479df6c81
JynxDelusions <joshua.breion@gmail.com>
2026-01-22 15:02:22 -0500

    Add files via upload

---
+++ FirstJava/src/First.java
@@ -0,0 +1,9 @@
+//Breion Joshua
+public class First {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola mundo.");
+        System.out.println("Job Title: Computer Programmer");
+        System.out.println("Why: Programming is my passion.");
+        System.out.println("Median Salary: $98,670 per year");
+    }
+}
