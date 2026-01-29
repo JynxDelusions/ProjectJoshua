3d1f7e62b88f80d4edb32039d658771331b85f4b
JynxDelusions <joshua.breion@gmail.com>
2026-01-29 13:18:53 -0500

    Add files via upload

---
+++ Tests.java
@@ -0,0 +1,15 @@
+public class Tests {
+    public static void main(String[] args) {
+
+        double Uno = 81.8;
+        double Dos = 92.9;
+        double Tres = 77.3;
+        double Promedio = (Uno + Dos + Tres) / 3;
+
+        System.out.println("Test score 1 is: " + Uno);
+        System.out.println("Test score 2 is: " + Dos);
+        System.out.println("Test score 3 is: " + Tres);
+        System.out.println("Average test score is: " + Promedio);
+    }
+
+}
